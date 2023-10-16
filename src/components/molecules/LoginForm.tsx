7bafba47e0bbcbe61c5132daeba8fc9e0918b528
takayasu <a.takayasu@gmail.com>
2023-10-17 02:30:04 +0900

    別の書き方

--- src/components/molecules/LoginForm.tsx
+++ src/components/molecules/LoginForm.tsx
@@ -18,16 +18,20 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 import { useForm, Controller } from "react-hook-form"
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert"
 
 const LoginForm = () => {
     const {
         control,
         formState: { errors },
         handleSubmit,
-    } = useForm({ defaultValues: { 
-        id: "",
-        password: "",
-        tenant: "tenant1" } });
+    } = useForm({
+        defaultValues: {
+            id: "",
+            password: "",
+            tenant: "tenant1"
+        }
+    });
 
     const onSubmit = (data) => {
         console.log(data);
@@ -47,17 +51,43 @@ const LoginForm = () => {
                         <div className="grid items-center w-full gap-4">
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="mail">ID(メールアドレス)</Label>
-                                <Controller name="id" control={control} render={({ field }) => {
-                                    console.log(field);
-                                    return (
-                                    <Input {...field} placeholder="xxx@example.com" />
-                                )}} />
+                                <Controller name="id" control={control}
+                                    rules={{
+                                        required: "Emailは必須です",
+                                        pattern: {
+                                            value: /\S+@\S+\.\S+/,
+                                            message: "Emailの形式で入力してください",
+                                        },
+                                    }}
+                                    render={({ field }) => {
+                                        return (
+                                            <Input {...field} placeholder="xxx@example.com" />
+                                        )
+                                    }} />
+                                {errors.id &&
+                                    <Alert>
+                                        <AlertTitle className="text-red-400">{errors.id.message}</AlertTitle>
+                                    </Alert>
+                                }
                             </div>
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="password">パスワード</Label>
-                                <Controller name="password" control={control} render={({ field }) => (
+                                <Controller name="password" control={control} 
+                                    rules={{
+                                        required: "パスワードは必須です",
+                                        minLength: {
+                                            value: 8,
+                                            message: "パスワードは8文字以上で入力してください",
+                                        },
+                                    }}
+                                    render={({ field }) => (
                                     <Input {...field} id="password" type="password" />
                                 )} />
+                                {errors.password &&
+                                    <Alert>
+                                        <AlertTitle className="text-red-400">{errors.password.message}</AlertTitle>
+                                    </Alert>
+                                }
                             </div>
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="tenant">所属組織</Label>
